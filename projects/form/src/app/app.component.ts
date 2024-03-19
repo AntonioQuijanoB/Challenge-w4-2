d9296c6553e1caa4e93b9c5a661f1ea63e5cf764
Antonio Quijano <antonio.quijanoberne@gmail.com>
2024-03-19 18:59:47 +0100

    Add new files

--- projects/form/src/app/app.component.ts
+++ projects/form/src/app/app.component.ts
@@ -1,12 +1,14 @@
+/* eslint-disable new-cap */
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './feature/form/form.component';
 
 @Component({
   selector: 'isdi-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'form';
---
+++ projects/form/src/app/feature/accessdata/accessdata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isdi-accessdata',
+  standalone: true,
+  imports: [],
+  templateUrl: './accessdata.component.html',
+  styleUrl: './accessdata.component.css'
+})
+export class AccessdataComponent {
+
+}
